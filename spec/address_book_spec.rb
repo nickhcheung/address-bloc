d791bab979db5c7df51dcff3074a981f6569369a
Nick Cheung <nickhcheung@hotmail.com>
2017-04-21 12:33:31 -0700

    Add tests and method for reading CSV's

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -3,6 +3,12 @@ require_relative "../models/address_book"
 RSpec.describe AddressBook do
   let(:book) { AddressBook.new }
 
+  def check_entry(entry, expected_name, expected_number, expected_email)
+    expect(entry.name).to eq expected_name
+    expect(entry.phone_number).to eq expected_number
+    expect(entry.email).to eq expected_email
+  end
+
   describe "attributes" do
     it "responds to entries" do
       expect(book).to respond_to(:entries)
@@ -39,6 +45,50 @@ RSpec.describe AddressBook do
       book.remove_entry('Ada LoveLace', '010.012.1815', 'augusta.king@lovelace.com')
 
       expect(book.entries.size).to eq(0)
+    end
+  end
+
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 5
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[0]
+
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[1]
+
+      check_entry(entry_one, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[2]
+
+      check_entry(entry_one, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+
+    it "imports the 4th entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[3]
+
+      check_entry(entry_one, "Sally", "555-555-4646", "sally@blocmail.com")
+    end
+
+    it "imports the 5th entry" do
+      book.import_from_csv("entries.csv")
+      entry_one = book.entries[4]
+
+      check_entry(entry_one, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
   end
 
